b58a799fbb0919c47522039445f29311c602d5e4
wwb <979717822@qq.com>
2019-11-14 00:17:05 +0800

    制作登陆页面

---
+++ src/components/Login.vue
@@ -0,0 +1,107 @@
+<template>
+<div class="login_container">
+<div class="login_box">
+<!--  头像区域-->
+  <div class="avatar_box">
+    <img alt="" src="../assets/logo.png">
+  </div>
+<!--  登录表单区域-->
+  <el-form :model="loginForm" :rules="loginFormRules" class="login_form" ref="loginFormRef">
+<!--    用户名-->
+    <el-form-item prop="username">
+      <el-input prefix-icon="el-icon-user" v-model="loginForm.Username" ></el-input>
+    </el-form-item>
+<!--    密码-->
+    <el-form-item prop="password">
+      <el-input prefix-icon="el-icon-s-goods" type="password" v-model="loginForm.Password" ></el-input>
+    </el-form-item>
+    <el-form-item  class="btns">
+    <el-button type="primary">登录</el-button>
+    <el-button @click="resetLoginForm" type="info">重置</el-button>
+    </el-form-item>
+   </el-form>
+</div>
+</div>
+</template>
+
+<script>
+
+    export default {
+        name: "Login",
+        data() {
+            return {
+                // 这是登录表单的数据绑定对象
+        loginForm: {
+                    Username: '',
+                    Password: ''
+                },
+                // 这是表单的验证规则对象
+        loginFormRules:{
+                    // 验证用户名是否合法
+                   username:[
+                        // { required: true, message: '请输入用户名', trigger: 'blur' },
+                        // { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'change' }
+                    ],
+                    // 验证密码是否合法
+                    password:[
+                        // { required: true, message: '请输入登录密码', trigger: 'blur' },
+                        // { min: 6, max:15, message: '长度在 6 到 15 个字符', trigger: 'change' }
+                    ]
+                }
+            }
+        },
+        // 未完成
+        methods:{
+            resetLoginForm(){
+                console.log(this)
+                this.$refs.loginFormRef.resetFields;
+            }
+        },
+    };
+</script>
+
+<style lang="less" scoped>
+.login_container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color:#fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  .avatar_box{
+    height: 130px;
+    width:130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left:50%;
+    background-color: #ffffff;
+    transform: translate(-50%,-50%);
+    img{
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: aqua;
+    }
+  }
+}
+.btns{
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
